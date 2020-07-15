9a3652c51b07f319cf9fadd74e2a5bbd924119f4
Paola Barboglio <paola.barboglio@gmail.com>
2020-07-15 12:32:13 +0200

    display shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Sugar', 100),
+    new Ingredient ('Flour', 200),
+    new Ingredient ('Milk', 150)
+  ];
   constructor() { }
 
   ngOnInit(): void {
