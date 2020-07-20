2da7d69a5c13597c14b62ca7d7efdb12939a2191
Paola Barboglio <paola.barboglio@gmail.com>
2020-07-20 17:02:40 +0200

    use less insane naming and add imagePath + alt name to image in details

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() detailedRecipe: Recipe;
+  @Input() recipe: Recipe;
   
   constructor() { }
 
